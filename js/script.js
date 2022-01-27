47b8436a5ec712e3142dab42242c3ad793c1b053
marcodeiulio <mdeiulio@icloud.com>
2022-01-27 16:53:08 +0100

    - Connected Vue devtools - Added v-if in html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 console.log('JS OK');
 
+Vue.config.devtools = true
+
 const app = new Vue({
 	el: '#root',
 	data: {
-		message: 'Hello Vue!',
+		currentImage: 0,
 		images: [
 			'images/image1.jpg',
 			'images/image2.jpg',
