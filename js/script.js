7f79647bf1280ed8e39cf8b6a5ed7115cee71c12
marcodeiulio <mdeiulio@icloud.com>
2022-01-27 16:49:20 +0100

    - Added images in js - Added images in html

--- js/script.js
+++ js/script.js
@@ -4,5 +4,11 @@ const app = new Vue({
 	el: '#root',
 	data: {
 		message: 'Hello Vue!',
+		images: [
+			'images/image1.jpg',
+			'images/image2.jpg',
+			'images/image3.jpg',
+			'images/image4.jpg',
+		]
 	}
 })
